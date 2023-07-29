f583c3e463220657fb227419caf0bdfc4828033c
jong-k <fully.charged07@gmail.com>
2023-07-29 17:05:58 +0900

    add: greedy 5번 문제 풀이과정 작성

--- greedy/q05.py
+++ greedy/q05.py
@@ -1,11 +1,15 @@
 N = 5
 VALUES = "2 3 1 2 2"
-
-
 # VALUES = 3 4 이면 0 출력
 
+"""
+VALUES를 오름차순으로 정렬한다
+맨 뒤의 가장 큰 숫자를 pop 한다
+popped - 1 만큼 VALUES에서 pop 반복 (이 때 VALUES에 남은 원소가 없으면 반복을 종료하고 결과를 반환)
+"""
+
+
 def solution(values):
-    """일단 정렬시키기"""
     sorted_values = sorted(map(int, values.split(" ")))
     print(sorted_values)
     result = 0
