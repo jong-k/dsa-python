0b640aec3bc42beb7e9c83f845b8cef0fe41f8ba
jong-k <fully.charged07@gmail.com>
2023-08-03 19:57:19 +0900

    add: 만들 수 없는 금액 문제풀이

---
+++ greedy/q08.py
@@ -0,0 +1,27 @@
+"""
+p.314 만들 수 없는 금액
+"""
+
+N = 5
+COINS = "3 2 1 1 9"
+# COINS = "1 2 5"
+
+"""풀이
+동전들을 작은 순으로 정렬하면 1 1 2 3 9
+동전들 중에 1은 반드시 포함되어 있어야 한다.
+따라서 앞, 뒤 순으로 동전이 있을 때, 뒤의 동전은 앞의 동전 +1 
+"""
+
+
+def solution(coins):
+    sorted_coins = sorted(map(int, coins.split(" ")))
+    possible_amount = 1
+    for coin in sorted_coins:
+        if coin > possible_amount:
+            break
+        else:
+            possible_amount += coin
+    return possible_amount
+
+
+print(solution(COINS))
