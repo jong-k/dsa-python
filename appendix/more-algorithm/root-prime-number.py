21f30190c7450426a6bc110ef7eeb29d6bde36b2
jong-k <fully.charged07@gmail.com>
2023-09-10 14:15:00 +0900

    add: 소수 판별 함수 구현

---
+++ appendix/more-algorithm/root-prime-number.py
@@ -0,0 +1,17 @@
+import math
+
+
+def is_prime_number(n):
+    """
+    n이 소수인지 판별하는 함수
+    :param n: 2이상의 자연수
+    :return: boolean
+    """
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+        return True
+
+
+print(is_prime_number(4))  # False
+print(is_prime_number(7))  # True
