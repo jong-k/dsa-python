76d198fc9a9e268d617192d4a2eaa8175a727211
jong-k <fully.charged07@gmail.com>
2023-10-12 00:32:51 +0900

    add: [최소신장트리] 최소신장트리 알고리즘 구현

---
+++ more-graphs/kruskal.py
@@ -0,0 +1,32 @@
+vertex_num = 7
+edge_num = 9
+edges = [(1, 2, 29), (1, 5, 75), (2, 3, 35), (2, 6, 34), (3, 4, 7), (4, 6, 23), (4, 7, 13), (5, 6, 53), (6, 7, 25)]
+root_list = [x for x in range(vertex_num + 1)]
+total_cost = 0  # 최종 비용
+
+
+def find_root(root, x):
+    if root[x] != x:
+        root[x] = find_root(root, root[x])
+    return root[x]
+
+
+def union_root(root, node1, node2):
+    root_node1 = find_root(root, node1)
+    root_node2 = find_root(root, node2)
+    if root_node1 < root_node2:
+        root[root_node2] = root_node1
+    else:
+        root[root_node1] = root_node2
+
+
+edges.sort(key=lambda x: x[2])  # 비용 기준 오름차순 정렬
+
+for start, end, cost in edges:
+    # 사이클이 발생하지 않는 간선만 집합에 포함
+    # 루트 노드가 같으면 사이클이 발생한 것으로 봄
+    if find_root(root_list, start) != find_root(root_list, end):
+        union_root(root_list, start, end)
+        total_cost += cost
+
+print(total_cost)  # 159
